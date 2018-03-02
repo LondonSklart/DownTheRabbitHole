5a249e99f80573ca61de9c4c45925dd57e89a254
Affe-R <ac.ruckle@gmail.com>
2018-03-02 15:31:22 +0100

    TestCommit
    Hope no break

--- Rabbjt/Assets/Scripts/Enemy/Encounter.cs
+++ Rabbjt/Assets/Scripts/Enemy/Encounter.cs
@@ -2,15 +2,8 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Encounter : MonoBehaviour {
+[CreateAssetMenu(fileName = "EncounterData", menuName = "Encounter/New Encounter", order = 1)]
+public class Encounter : ScriptableObject {
 
-	// Use this for initialization
-	void Start () {
-		
-	}
-	
-	// Update is called once per frame
-	void Update () {
-		
-	}
+	public List<GameObject> enemiesInRoom;
 }
